7ef567781711ac11ca1197155990c09370410fd2
NikitaSempai <nik.0302@mail.ru>
2023-04-17 23:52:35 +0300

    first

---
+++ src/main/java/com/homework/skypro/SkyproApplication.java
@@ -0,0 +1,13 @@
+package com.homework.skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproApplication.class, args);
+	}
+
+}
